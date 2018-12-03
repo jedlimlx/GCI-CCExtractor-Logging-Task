722ead978ce8a3476768ed37453e7fb2449f380f
jedlimlx <42632089+jedlimlx@users.noreply.github.com>
2018-12-03 21:23:39 +0800

    Update Logging.py

--- Logging.py
+++ Logging.py
@@ -7,10 +7,10 @@ logging.basicConfig(filename='demo.log')
 coloredlogs.install(level='DEBUG', logger=logger,
                     fmt='%(asctime)s %(hostname)s %(name)s[%(process)d] %(levelname)s %(message)s',
                     level_styles={'info': {'color' : 'yellow', 'bold' : True},
-                                  'warning': {'color': 'red'}})
+                                  'debug': {'color': 'red'}})
 
 # Yellow Logger Info
 logger.info("INFORMATION")
 
 # Output to a File
-logger.warn("WARNING!!")
+logger.debug("DEBUGGING")
